7e01deb2200412c56731e11f3ce5f157dd0d6fcd
Muhammad Zachrie Kurniawan <zachrie2005@gmail.com>
2023-09-29 16:32:27 +0700

    feat(main): add button interaction on click and calculate the result

--- app/src/main/java/com/example/challenge_2/MainActivity.kt
+++ app/src/main/java/com/example/challenge_2/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.challenge_2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import com.example.challenge_2.databinding.ActivityMainBinding
+import kotlin.math.ceil
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener {
+            val costOfService = binding.costOfService.text.toString().toDoubleOrNull() ?: 0.0
+            val roundUp = binding.roundUpSwitch.isChecked
+            val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
+                binding.optionTwentyPercent.id -> 0.20
+                binding.optionEighteenPercent.id -> 0.18
+                binding.optionFifteenPercent.id -> 0.15
+                else -> 0.0
+            }
+            var tipAmount = costOfService * tipPercentage
+            if (roundUp) {
+                tipAmount = ceil(tipAmount)
+            }
+
+            binding.tipResult.text = tipAmount.toString()
+        }
     }
 }
